84d474fb8ef5e7fe2367c36ec7879ba73b6e4404
Archyved <estebannikkob@gmail.com>
2024-03-15 01:23:43 +0800

    Added Put and Delete Endpoint

--- server-task-manager/src/routes/task.ts
+++ server-task-manager/src/routes/task.ts
@@ -5,25 +5,62 @@ taskRoutes
   .get('/', async (req: Request, res: Response) => {
     console.log("RETRIEVING TASKS")
     const Q = 'SELECT * FROM tasks';
-    req.mysqlConnection.query(Q, [req.body.name], (err, results) => {
-      if (err)
+    req.mysqlConnection.query(Q, [req.body.title], (err, results) => {
+      if (err) {
         res.status(500).send("Unknown Error Occurred")
+        return
+      }
       res.send({ data: results, error: null })
     })
   })
   .post('/', async (req: Request, res: Response) => {
     console.log("ADDING TASKS")
-    const Q = 'INSERT INTO tasks (name) VALUES (?)';
+    const Q = 'INSERT INTO tasks (title) VALUES (?)';
 
-    if (!req.body.name) {
-      res.send({ data: null, error: 'Task Name is required' })
+    if (!req.body.title) {
+      res.send({ data: null, error: 'Task Title is required' })
+      return
     }
 
-    req.mysqlConnection.query(Q, [req.body.name], (err, results) => {
-      if (err)
+    req.mysqlConnection.query(Q, [req.body.title], (err, results) => {
+      if (err) {
         res.status(500).send("Unknown Error Occurred")
+        return
+      }
       res.send({ data: null, error: null })
     })
   })
+  .delete('/:task_id', (req: Request, res: Response) => {
+    console.log("DELETING TASK")
+    if (req.params.task_id == undefined) {
+      res.send({ data: null, error: 'Task ID is required' })
+      return
+    }
+    const Q = 'DELETE FROM tasks WHERE id = ?';
+    req.mysqlConnection.query(Q, [req.params.task_id], (err, results) => {
+      if (err) {
+        res.status(500).send("Unknown Error Occurred")
+        return
+      }
+      res.send({ data: null, error: null })
+    })
+  })
+  .put('/:task_id', (req: Request, res: Response) => {
+    console.log("UPDATING TASK")
+    const taskID = req.params.task_id;
+    const status = req.body.statusCompleted
+    if (!taskID) {
+      res.status(400).send({ data: null, error: 'Task ID is required' });
+      return;
+    }
+    const Q = 'UPDATE tasks SET statusCompleted = ? WHERE id = ?';
+    req.mysqlConnection.query(Q, [status, taskID], (err, results) => {
+      if (err) {
+        res.status(500).send({ data: null, error: "Unknown Error Occurred" });
+        return;
+      }
+      res.send({ data: null, error: null });
+    });
+  });
 
 export default taskRoutes
