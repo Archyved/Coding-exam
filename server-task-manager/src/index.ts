673d0003cfc7f016cc1f85c9fa2a0e0bbe9dc9fc
Archyved <estebannikkob@gmail.com>
2024-03-15 00:18:15 +0800

    Simple Fetch

--- server-task-manager/src/index.ts
+++ server-task-manager/src/index.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import { Request, Response } from 'express';
 import taskRoutes from './routes/task'
 import Database from '../model/index'
+import bodyParser from 'body-parser'
 const app = express();
 const port = 8081;
 
 
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(Database);
 app.get('/', async (req: Request, res: Response) => {
   res.send('Application Started');
--- server-task-manager/src/routes/task.ts
+++ server-task-manager/src/routes/task.ts
@@ -1,8 +1,23 @@
 import express, { Request, Response } from "express"
 const taskRoutes = express.Router()
-taskRoutes.get('/', async (req: Request, res: Response) => {
-  res.send('Get Tasks')
-})
+taskRoutes
 
+  .get('/', async (req: Request, res: Response) => {
+    const Q = 'SELECT * FROM tasks';
+    req.mysqlConnection.query(Q, [req.body.name], (err, results) => {
+      if (err)
+        console.log(err)
+      res.send({ data: results, error: null })
+    })
+  })
+  .post('/', async (req: Request, res: Response) => {
+    const Q = 'INSERT INTO tasks (name) VALUES (?)';
+    req.mysqlConnection.query(Q, [req.body.name], (err, results) => {
+      if (err)
+        console.log(err)
+      console.log(results)
+    })
+    res.send({ error: null })
+  })
 
 export default taskRoutes
